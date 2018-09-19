6c445be5f3d110447e1af9a2766ece9510581b97
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 12:18:02 -0700

    Solved Stack for shared methods version

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  
+  
+  _.extend(instance, stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val) {
+    var stackLength = this.size();
+    this.storage[stackLength] = val;
+  },
+  pop: function() {
+    var topIdx = this.size() - 1;
+    var topVal = this.storage[topIdx];
+    
+    delete this.storage[topIdx];
+    return topVal;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
