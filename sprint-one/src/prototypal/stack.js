a260d670dfcd56e0f29661879dab35ce93ac1f5a
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 12:35:08 -0700

    Solved stack for Prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val) {
+    var stackSize = this.size();
+    this.storage[stackSize] = val;
+  },
+  pop: function() {
+    var topIdx = this.size() - 1;
+    var topVal = this.storage[topIdx];
+    delete this.storage[topIdx];
+    return topVal;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
