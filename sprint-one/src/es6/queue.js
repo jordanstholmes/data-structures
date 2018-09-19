0e455dad7b107197afab93a3435129b22af6a7b5
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 14:47:12 -0700

    Solved for es6 versions

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
-
+  enqueue(val) {
+    var keys = Object.keys(this.storage);
+    var nextKey = keys.length === 0 ? 0 : keys[keys.length - 1] + 1;
+    this.storage[nextKey] = val;
+  }
+  dequeue() {
+    var keys = Object.keys(this.storage);
+    if (keys.length === 0) { return; }
+    var firstKey = keys[0];
+    var firstVal = this.storage[firstKey];
+    delete this.storage[firstKey];
+    return firstVal; 
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,19 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
-
+  push(val) {
+    var stackSize = this.size();
+    this.storage[stackSize] = val;
+  }
+  pop() {
+    var topIdx = this.size() - 1;
+    var topVal = this.storage[topIdx];
+    delete this.storage[topIdx];
+    return topVal;
+  }
+  size() {
+    return Object.keys(this.storage).length; 
+  }
 }
