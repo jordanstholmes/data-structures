a509197b31047abb0852a41d4cb5a94353abc0ab
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 13:09:52 -0700

    Solved through prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val) {
+    var keys = Object.keys(this.storage);
+    var lastKey = keys[keys.length - 1];
+    var nextIdx = keys.length === 0 ? 0 : lastKey + 1;
+    this.storage[nextIdx] = val;
+  },
+  dequeue: function() {
+    var keys = Object.keys(this.storage);
+    if (keys.length === 0) { return; }
+    
+    var firstIdx = keys[0];
+    var firstVal = this.storage[firstIdx];
+    delete this.storage[firstIdx];
+    return firstVal;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
