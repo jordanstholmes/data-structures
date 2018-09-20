4c8a9c87c493b1a4545bda964bb77dff7326e333
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 20:11:56 -0700

    Solved up to Graph

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+/*
+add new tree to current tree's children
+
+*/
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+//check value of current object, if matches then return true
+// if no children return false
+// return result of checking children
+  if(this.value === target) {
+    return true;
+  } else if (this.children.length === 0) {
+    return false;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
