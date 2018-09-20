666286be198784e5de5e37b50379a6399c76077d
jordanstholmes <jordanstholmes@gmail.com>
2018-09-19 17:39:42 -0700

    Solved up to Graph

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+
+    if (list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    } else {
+      list.tail.next = newTail;
+      list.tail = newTail; 
+    }
   };
 
   list.removeHead = function() {
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+
   };
 
   return list;
