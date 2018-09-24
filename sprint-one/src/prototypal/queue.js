9d7e378ebfeae7fe6c984dbe1b69d48b56bef23d
Jordan Holmes <jordanstholmes@gmail.com>
2018-09-24 08:09:58 -0700

    Cleaned up queue in prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,30 +1,33 @@
 var Queue = function() {
   var instance = Object.create(queueMethods);
-  instance.storage = {};
+
+  instance._storage = {};
+  instance._start = 0;
+  instance._end = 0;
+
   return instance;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {
+
   enqueue: function(val) {
-    var keys = Object.keys(this.storage);
-    var lastKey = keys[keys.length - 1];
-    var nextIdx = keys.length === 0 ? 0 : lastKey + 1;
-    this.storage[nextIdx] = val;
+    this._storage[this._end++] = val;
   },
+
   dequeue: function() {
-    var keys = Object.keys(this.storage);
-    if (keys.length === 0) { return; }
-    
-    var firstIdx = keys[0];
-    var firstVal = this.storage[firstIdx];
-    delete this.storage[firstIdx];
-    return firstVal;
+    var result = this._storage[this._start];
+    delete this._storage[this._start];
+
+    this.size() && this._start++;
+    return result;
   },
+
   size: function() {
-    return Object.keys(this.storage).length;
+    return this._end - this._start;
   }
+
 };
 
 
