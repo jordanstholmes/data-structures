0a45375166537290a4378a02ebd56ba71e2dd927
Jordan Holmes <jordanstholmes@gmail.com>
2018-09-23 21:50:30 -0700

    Changed functional stack based off of solution

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,23 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  someInstance.storage = storage;
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    var currentLen = someInstance.size();
-    storage[currentLen] = value;
+    storage[size++] = value;
   };
 
   someInstance.pop = function() {
-    var topIdx = someInstance.size() - 1;
-    var topValue = storage[topIdx];
-    delete storage[topIdx]; 
+    if (size) {
+      var topValue = storage[--size];
+      delete storage[size];
+    }
+     
     return topValue;
   };
 
   someInstance.size = function() {
-    return Object.keys(storage).length;
+    return size;
   };
 
   return someInstance;
